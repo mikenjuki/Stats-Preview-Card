c12fbb28005ffa9971b74743f3e283b42a6fe19a
Eric Njuki <23366230+ericnjuki@users.noreply.github.com>
2023-01-21 14:04:30 +0300

    desktop design done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,17 @@ module.exports = {
   theme: {
     screens: {
       sm: "375px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1440px",
+      md: "1440px",
     },
     extend: {
       colors: {
         veryDarkBlue: "hsl(233, 47%, 7%)",
         darkUnsaturatedBlue: "hsl(244, 38%, 16%)",
         softViolet: "hsl(277, 64%, 61%)",
-        softVioletBlur: "rgba(170, 92, 219, 0.5)",
+        softVioletBlur: "rgb(117 0 190 / 54%)",
         mainWhite: "hsl(0, 0%, 100%)",
         mainTransparentWhite: "hsla(0, 0%, 100%, 0.75)",
         statTransparentWhite: "hsla(0, 0%, 100%, 0.6)",
-      },
-      hueRotate: {
-        300: '300deg',
       },
     },
     fontFamily: {
